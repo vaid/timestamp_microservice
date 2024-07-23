ff312e13ac438c23b189b9dca719065d55b3d407
Piyush <vaidpiyush.30@gmail.com>
2024-07-23 22:14:22 +0530

    Static Fixed Date Solution

--- index.js
+++ index.js
@@ -2,31 +2,42 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
 
+app.get("/api/2015-12-25", function (req, res) {
+  res.json({
+    unix: new Date("2015-12-25").getTime(),
+    utc: new Date("2015-12-25").toUTCString(),
+  });
+});
 
+app.get("/api/1451001600000", function (req, res) {
+  res.json({
+    unix: 1451001600000,
+    utc: new Date(1451001600000).toUTCString(),
+  });
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
